295b015b3b33aa35996cd2d97ef922f0478635c6
phbridge <bridges.p.j1@gmail.com>
2019-10-24 17:26:55 +0100

    Added monitoring of QoS "PLAT/GOLD/SILVER/BRONZE/TIN/Default"

--- Grafana_Router_NAT_Stats.py
+++ Grafana_Router_NAT_Stats.py
@@ -230,7 +230,7 @@ def login_to_host(seed_hostname, seed_username, seed_password, device_OS):
         qos_output_raw = []
         for line in qos_output_raw_raw:
             if not "        " in line:
-                qos_output_raw.append(line)
+                qos_output_raw += line
 
         QoS_PLAT_Pkts = qos_output_raw.splitlines()[-7].split(" ")[-4]
         results += 'QoS_PLAT_IN_Pkts{host="%s"} %s\n' % (seed_hostname, str(QoS_PLAT_Pkts))
