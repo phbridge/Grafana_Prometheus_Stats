8db4873e980cca15ebf195a1c29103d818bf090b
phbridge <bridges.p.j1@gmail.com>
2021-10-12 10:37:13 +0100

    updated ip and ipv6 section logging

--- Grafana_Router_NAT_Stats.py
+++ Grafana_Router_NAT_Stats.py
@@ -171,12 +171,12 @@ def get_total_v4_v6_split(session, os_type, seed_hostname, interface, influx=Tru
     ip_output = ""
     ipv6_output = ""
     try:
-        ip_output = run_command(session, "sho ip traffic interface %s" % interface, 1)
-        ipv6_output = run_command(session, "sho ipv6 traffic  interface %s" % interface, 1)
+        ip_output = run_command(session, "sho ip traffic interface %s" % interface, 3)
+        ipv6_output = run_command(session, "sho ipv6 traffic  interface %s" % interface, 3)
         if os_type == "IOS-XE":
             if len(ip_output.splitlines()) > 16:
-                function_logger.info(ip_output.splitlines()[14])
-                function_logger.info(ip_output.splitlines()[5])
+                function_logger.info("host=%s %s" % (seed_hostname, ip_output.splitlines()[14]))
+                function_logger.info("host=%s %s" % (seed_hostname, ip_output.splitlines()[5]))
                 ip_pkts_sent = int(ip_output.splitlines()[14].split()[1])
                 ip_bytes_sent = int(ip_output.splitlines()[14].split()[3])
                 ip_pkts_rcvd = int(ip_output.splitlines()[5].split()[1])
@@ -196,8 +196,8 @@ def get_total_v4_v6_split(session, os_type, seed_hostname, interface, influx=Tru
                     results += 'ip_pkts_rcvd{host="%s"} %s\n' % (seed_hostname, str(ip_pkts_rcvd))
                     results += 'ip_bytes_rcvd{host="%s"} %s\n' % (seed_hostname, str(ip_bytes_rcvd))
             if len(ipv6_output.splitlines()) > 16:
-                function_logger.info(ip_output.splitlines()[14])
-                function_logger.info(ip_output.splitlines()[5])
+                function_logger.info("host=%s %s" % (seed_hostname, ipv6_output.splitlines()[14]))
+                function_logger.info("host=%s %s" % (seed_hostname, ipv6_output.splitlines()[5]))
                 ipv6_pkts_sent = int(ipv6_output.splitlines()[14].split()[1])
                 ipv6_bytes_sent = int(ipv6_output.splitlines()[14].split()[3])
                 ipv6_pkts_rcvd = int(ipv6_output.splitlines()[5].split()[1])
